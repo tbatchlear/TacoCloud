d609259e16636c3375df2000d22a8263348975ee
Tom Batchlear <tom.j.batchlear@boeing.com>
2023-01-24 15:17:18 -0800

    chapter 2 - 2.2 - processing form submission - updated to handle post requests containing Taco and return an order form after taco creation

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -1,12 +1,13 @@
 package tacos;
 
-import lombok.Data;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
+
+import lombok.Data;
 
 @Data
 public class TacoOrder {
+
   private String deliveryName;
   private String deliveryStreet;
   private String deliveryCity;
